eea03123347f2ac2dd804e16b01e984b43bf445c
Jeroen Ooms <jeroenooms@gmail.com>
2016-08-12 10:41:20 +0200

    Sync with opensl

--- src/nslookup.c
+++ src/nslookup.c
@@ -7,11 +7,10 @@
 const char *inet_ntop(int af, const void *src, char *dst, socklen_t size);
 #else
 #include <sys/socket.h>
+#include <netinet/in.h>
+#include <resolv.h>
 #include <netdb.h>
 #include <arpa/inet.h>
-#ifdef __FreeBSD__
-#include <netinet/in.h>
-#endif
 #endif
 
 SEXP R_nslookup(SEXP hostname) {
